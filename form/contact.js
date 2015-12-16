46d21f869443199e697503eb4e7688664e5819aa
pablo archanco alonso <pabloarchanco@hotmail.com>
2015-12-16 11:41:12 +0100

    Formulario verificacion js y HTML5 terminada

--- form/contact.js
+++ form/contact.js
@@ -56,3 +56,49 @@ function habilitarchk() {
     document.getElementById("submit").disabled = (document.getElementById("conditions").checked) ? false : true;
 
 }
+/**
+ * Funcion que comprueba si el parametro imput introducido cumple el patron
+ * @param regexp
+ * @param input
+ * @returns {boolean}
+ */
+function chk(regexp, input) {
+    if (!regexp.test(input)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function validar() {
+    /************ CAMPOS A COMPROBAR ************/
+    var nombre = document.getElementById("name").value;
+    var apellido1 = document.getElementById("surname1").value;
+    var apellido2 = document.getElementById("surname2").value;
+    var email = document.getElementById("email").value;
+    var telefonoFijo = document.getElementById("phone").value;
+    var telefonoMovil = document.getElementById("mobil").value;
+
+    /************ EXPRESIONES REGULARES ************/
+    /************ expresion regular sting con espacio ************/
+    var regexString = /^([a-z ]{1,30})/i;
+    /************ expresion regular email ************/
+    var regexEmail = /^\w+@\w+?\.*\w{2,3}$/;
+    /************ expresion regular telefono fijo ************/
+    var regexTelefonoFijo = /^[9]\d{8}$/;
+    /************ expresion regular telefono movil ************/
+    var regexTelefonoMovil = /^[6]\d{8}$/;
+
+    /************ Pongo solo & para que compruebe todos los campos del if ************/
+    if (chk(regexString, nombre) &
+        chk(regexString, apellido1) &
+        chk(regexString, apellido2) &
+        chk(regexEmail, email) &
+        chk(regexTelefonoFijo, telefonoFijo) &
+        chk(regexTelefonoMovil, telefonoMovil)) {
+        return true;
+    }
+    else {
+        alert('Error al enviar, el formulario compruebe los campos');
+        return false;
+    }
+}
