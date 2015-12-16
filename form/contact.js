e9af6d354344583aad0b4d04bb4f7286283e8ea1
pablo archanco alonso <pabloarchanco@hotmail.com>
2015-12-16 08:31:30 +0100

    actualizacion de fomulario

--- form/contact.js
+++ form/contact.js
@@ -23,57 +23,36 @@
  along with this program. If not, see <http://www.gnu.org/license/>
  */
 /**
- * verifica que el parametro es una cadena de caracteres
- * @param string
- * @returns {boolean}
- */
-function validateString(string) {
-    return (/^[a-z]+$/i.test(string))? true : false;
-}
-/**
- * verifica que el parametro que le pasa es un numero
- * @param number
- * @returns {boolean}
- */
-function validateNumber(number) {
-    return (/[0-9]{9}/.test(number)) ? true : false;
-}
-/**
- * verifica que el parametro es una cadena de caracteres de tipo email
- * @param string
- * @returns {boolean}
- */
-function validateMail(string) {
-    return (/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i.test(string)) ? true : false;
-}
-/**
  * funcion que comprueba si todos los campos estan rellenados y su longitud no es nula
  * de ser asi habilita el campo de terminos y condiciones
- * @param event
  */
-function habilitar(event) {
+function habilitar() {
     var array = new Array();
     array.push(document.getElementById("name").value.length);
     array.push(document.getElementById("surname1").value.length);
     array.push(document.getElementById("surname2").value.length);
     array.push(document.getElementById("email").value.length);
-    array.push(document.getElementById("phone").value.length);
     array.push(document.getElementById("mobil").value.length);
-    array.push(document.getElementById("textarea").value.length);
+    array.push(document.getElementById("texto").value.length);
+
     var camposRellenados = true;
-    for(var i = 0; i < array.length; i++)
+    for (var i = 0; i < array.length; i++) {
         if (Number(array[i]) <= 0) {
             camposRellenados = false;
             break;
         }
+    }
     if (camposRellenados) {
         document.getElementById("conditions").disabled = false;
-        document.getElementById("submit").disabled = false;
+
     } else {
-        // En caso que haya un campo vacio volvemos a ponerlo disabled
         document.getElementById("conditions").disabled = true;
+        document.getElementById("conditions").checked = false;
         document.getElementById("submit").disabled = true;
     }
-}
 
-function validar(){}
+}
+function habilitarchk() {
+    document.getElementById("submit").disabled = (document.getElementById("conditions").checked) ? false : true;
+
+}
